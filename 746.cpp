4bccd5e2a95ab36f8139e64aada67e4c18db0943
malvika-jindal <48233066+malvika-jindal@users.noreply.github.com>
2020-05-31 01:59:51 +0530

    simple dp problem

---
+++ 746.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int dp[cost.size()+1];
+        if(cost.size()==1)
+        {
+            return 0;
+        }
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2;i<cost.size();i++)
+        {
+            dp[i]=cost[i]+min(dp[i-1],dp[i-2]);
+        }
+        return min(dp[cost.size()-1],dp[cost.size()-2]);
+    }
+};
