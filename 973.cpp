8bd7a7a5d6084bbd2d50cae2076c4abf40c20bb3
malvika-jindal <48233066+malvika-jindal@users.noreply.github.com>
2020-06-10 14:13:30 +0530

    Simple sort ques

---
+++ 973.cpp
@@ -0,0 +1,23 @@
+// K Closest Points to Origin
+//288ms; better than 90% solutions in terms of time complexity
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        vector<pair<float,int>>v;
+        for(int i=0;i<points.size();i++)
+        {
+            float x=sqrt(points[i][0]*points[i][0]+points[i][1]*points[i][1]);
+            v.push_back({x,i});
+        }
+        sort(v.begin(),v.end());
+         vector<vector<int>>ans;
+        for(int i=0;i<K;i++)
+        {
+            ans.push_back(points[v[i].second]);
+        }
+                        return ans;
+    }
+}; 
